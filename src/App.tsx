60a8edd12b82e030ff6aa98690e196259a37cc20
vladji <vladjislav@gmail.com>
2023-03-20 22:24:13 +0600

    feat: privacy policy

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
+import { Home } from './pages/Home';
+import { PrivacyPolicy } from './pages/PrivacyPolicy';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/privacy-policy',
+    element: <PrivacyPolicy />,
+  },
+]);
 
 function App() {
   return (
-    <div>
-      <h1>eRates</h1>
-    </div>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
   );
 }
 
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './styles.module.scss';
+
+export const Home = () => {
+  return (
+    <section className={styles.wrapper}>
+      <h1>eRates</h1>
+    </section>
+  );
+};
